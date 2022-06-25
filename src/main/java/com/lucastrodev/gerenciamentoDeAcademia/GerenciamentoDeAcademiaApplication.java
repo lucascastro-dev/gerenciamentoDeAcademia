c5fed7327f7ec7c4806edaad725c2b8e92f21178
Lucas Castro <professorlucascastro@gmail.com>
2022-06-25 08:43:15 -0300

    Criando projeto Spring

---
+++ src/main/java/com/lucastrodev/gerenciamentoDeAcademia/GerenciamentoDeAcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.lucastrodev.gerenciamentoDeAcademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeAcademiaApplication.class, args);
+	}
+
+}
